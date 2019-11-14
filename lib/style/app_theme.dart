d93f9be021cfabde663df4c805f7e6c6f1fcc62c
KevKipkemoi <kevkipkemoi@outlook.com>
2019-11-14 11:53:26 +0300

    app material and theme

---
+++ lib/style/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const darkBlue = Color(0xFF292787);
+  static const blue = Color(0xFF3E3C93);
+  static const tealAccent = Color(0xFF10EDC5);
+  static const darkGreen = Color(0xFF1B5E20);
+  static const pinkAccent = Color(0xFFED1946);
+  static const brightPink = Color(0xFFFF4081);
+  static const blueGrey = Color(0xFF9A9AAC);
+  static const brightGreen = Color(0xFF00FF00);
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const facebookColor = Color(0xFF3B5998);
+  static const googleColor = Color(0xFFDB4437);
+  static const transparent = Colors.transparent;
+  static const lightBlue = Colors.lightBlue;
+  static const red = Colors.red;
+  static const soulRed = Color(0xFFA80112);
+  static const fadedRed = Color(0xa0d66a69);
+}
