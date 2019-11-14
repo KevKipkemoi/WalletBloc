a6c3a71179a3185c9713b26a8c4af899c887dbee
KevKipkemoi <kevkipkemoi@outlook.com>
2019-11-14 12:49:53 +0300

    ThemeData

--- lib/style/app_theme.dart
+++ lib/style/app_theme.dart
@@ -18,4 +18,62 @@ class AppTheme {
   static const red = Colors.red;
   static const soulRed = Color(0xFFA80112);
   static const fadedRed = Color(0xa0d66a69);
+
+  static const _bgLeftColor = Color(0xFF330867);
+  static const _bgRightColor = Color(0xFF30cfd0);
+
+  static ThemeData appTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: MaterialColor(0xFF292787, <int, Color>{
+      50: Color(0xff9493c3),
+      100: Color(0xff7e7db7),
+      200: Color(0xff6967ab),
+      300: Color(0xff53529f),
+      400: Color(0xff3e3c93),
+      500: Color(0xff292787),
+      600: Color(0xff242379),
+      700: Color(0xff201f6c),
+      800: Color(0xff1c1b5e),
+      900: Color(0xff181751),
+    }),
+    primaryColor: white,
+    primaryColorDark: Color(0xFF181751),
+    accentColor: tealAccent,
+    canvasColor: Colors.white,
+    textTheme: TextTheme(
+      display1: TextStyle(color: Colors.white),
+      display2: TextStyle(color: Colors.white),
+      display3: TextStyle(color: Colors.white),
+      display4: TextStyle(color: Colors.white),
+      headline: TextStyle(color: Colors.white),
+      button: TextStyle(color: Colors.white),
+      body1: TextStyle(color: Colors.white),
+      body2: TextStyle(color: Colors.white),
+      title: TextStyle(color: Colors.white),
+      caption: TextStyle(color: Colors.white),
+      subhead: TextStyle(color: Colors.white),
+    ),
+    primaryTextTheme: TextTheme(
+      display1: TextStyle(color: blueGrey),
+      display2: TextStyle(color: blueGrey),
+      display3: TextStyle(color: blueGrey),
+      display4: TextStyle(color: blueGrey),
+      headline: TextStyle(color: blueGrey),
+      button: TextStyle(color: blueGrey),
+      body1: TextStyle(color: blueGrey),
+      body2: TextStyle(color: blueGrey),
+      title: TextStyle(color: blueGrey),
+      caption: TextStyle(color: blueGrey),
+      subhead: TextStyle(color: blueGrey),
+    ),
+    primaryIconTheme: IconThemeData(
+      color: white,
+    ),
+    pageTransitionsTheme: PageTransitionsTheme(
+        builders: <TargetPlatform, PageTransitionsBuilder> {
+          TargetPlatform.android : _MyWalletPageTransitionBuilder(),
+          TargetPlatform.iOS : _MyWalletPageTransitionBuilder()
+        }
+    ),
+  );
 }
