31c53c201cb4d16f9c7f83b0218266cc3b1f07d9
KevKipkemoi <kevkipkemoi@outlook.com>
2019-11-18 14:31:19 +0300

    data observer

---
+++ lib/data/data_observer.dart
@@ -0,0 +1,21 @@
+import 'package:wallet_bloc/data/data_observer.dart' as db;
+
+const tableAccount = "table_accounts";
+const tableTransactions = "table_transactions";
+const tableCategory = "table_categories";
+const tableBudget = "table_budget";
+const tableUser = "table_user";
+const tableTransfer = "table_transfer";
+const tableDischargeLiability = "table_discharge_liability";
+
+abstract class DatabaseObservable {
+  void onDatabaseUpdate(String table);
+}
+
+void registerDatabaseObservable(List<String> tables, DatabaseObservable observable) {
+  db.registerDatabaseObservable(tables, observable);
+}
+
+void unregisterDatabaseObservable(List<String> tables, DatabaseObservable observable) {
+  db.unregisterDatabaseObservable(tables, observable);
+}
