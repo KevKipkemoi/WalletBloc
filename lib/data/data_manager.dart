20578e0be7979494f089ae462ae62e008f5abca9
KevKipkemoi <kevkipkemoi@outlook.com>
2019-11-18 12:17:24 +0300

    Account, Transaction and Category table

---
+++ lib/data/data_manager.dart
@@ -0,0 +1,31 @@
+
+// database manager
+final _id = "_id";
+final _updated = "_updated";
+
+// table Account
+final _tableAccounts = tableAccount;
+final _accName = "_name";
+final _accInitialBalance = "_initialBalance";
+final _accCreated = "_created";
+final _accType = "_type";
+final _accCurrency = "_currency";
+final _accBalance = "_balance";
+final _accSpent = "_spent";
+final _accEarned = "_earned";
+
+// table Transaction
+final _tableTransactions = tableTransactions;
+final _transDateTime = "_dateTime";
+final _transAcc = "_accountId";
+final _transCategory = "_categoryId";
+final _transAmount = "_amount";
+final _transDesc = "_transactionDescription";
+final _transType = "_transactionType";
+final _transUid = "_transactionUserUid";
+
+// table Category
+final _tableCategory = tableCategory;
+final _catName = "_name";
+final _catColorHex = "_colorHex";
+final _catCategoryType = "_type";
